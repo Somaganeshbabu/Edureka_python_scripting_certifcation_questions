990944b883d22813821142c18b87cd940e11f634
Ganesh babu Soma <36077023+Somaganeshbabu@users.noreply.github.com>
2024-05-22 13:11:40 +0000

    created new approach in this file

--- caseStudy_1/Count_Chars.py
+++ caseStudy_1/Count_Chars.py
@@ -1,14 +1,22 @@
-def char_counts(chars):
-    char_count={}
-    for char in chars:
-        if char in char_count:
-            char_count[char]+=1
-        else:
-            char_count[char] = 1
-    return char_count
+# def char_counts(chars):
+#     char_count={}
+#     for char in chars:
+#         if char in char_count:
+#             char_count[char]+=1
+#         else:
+#             char_count[char] = 1
+#     return char_count
 
-input_string = input("enter string to count:- ")
-print(char_counts(input_string))
+# input_string = input("enter string to count:- ")
+# print(char_counts(input_string))
 
-# enter string to count:- abcdefgabc
-# {'a': 2, 'b': 2, 'c': 2, 'd': 1, 'e': 1, 'f': 1, 'g': 1}
+# # enter string to count:- abcdefgabc
+# # {'a': 2, 'b': 2, 'c': 2, 'd': 1, 'e': 1, 'f': 1, 'g': 1}
+
+#----------another approach -------
+def char_count(name):
+    return {char:name.count(char) for char in name} #syntax : {key:value for item in iterable}
+
+name = input("enter name to count chars:- ")
+print(char_count(name))
+
